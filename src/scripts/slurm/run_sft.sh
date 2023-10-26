b8e82c8fcbcac83dfaf1d828ad5491d0824f281b
Luckeciano Carvalho Melo <lucelo@clpc404.cs.ox.ac.uk>
2023-10-26 14:00:39 +0100

    Fixing conda environment and slurm script for running on cloud

--- src/scripts/slurm/run_sft.sh
+++ src/scripts/slurm/run_sft.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:1
-#SBATCH --output=/home/lucelo/logs/slurm-%j.out
-#SBATCH --error=/home/lucelo/logs/slurm-%j.err
+#SBATCH --cpus-per-task=24
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name="sft"
+#SBATCH --output=/users/lucelo/logs/slurm-%j.out
+#SBATCH --error=/users/lucelo/logs/slurm-%j.err
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
 export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 export XDG_CACHE_HOME=/scratch-ssd/oatml/
 
 
-/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/UQLRM/environment.yml
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/UQLRM/environment_cloud.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate uqrm
 
+nvidia-smi
 
-srun python ~/UQLRM/src/scripts/sft.py \
+python ~/UQLRM/src/scripts/sft.py \
 --per_device_train_batch_size 1 \
 --per_device_eval_batch_size 1
