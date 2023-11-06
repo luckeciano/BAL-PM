12d98218affa8e50a7ab673ba6d7ac88e93fe040
Luckeciano Carvalho Melo <lucelo@clpc404.cs.ox.ac.uk>
2023-11-06 16:10:50 +0000

    Adding jupyter notebook to handle reddit tldr human preferences dataset

---
+++ src/scripts/slurm/run_notebook.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --job-name="jupyter"
+#SBATCH --output=/users/lucelo/logs/slurm-%j.out
+#SBATCH --error=/users/lucelo/logs/slurm-%j.err
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+export XDG_CACHE_HOME=/scratch-ssd/oatml/
+
+export TMPDIR=/scratch-ssd/${USER}/tmp
+mkdir -p $TMPDIR
+BUILD_DIR=/scratch-ssd/${USER}/conda_envs/pip-build
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/UQLRM/environment_cloud.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate uqrm
+
+echo $TMPDIR
+
+nvidia-smi
+
+jupyter notebook .
