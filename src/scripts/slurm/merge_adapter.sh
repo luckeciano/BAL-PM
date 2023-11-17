bef3c625dcbc34b2a8aa071cac28d5d9d8dc306b
Luckeciano Melo <luckeciano@gmail.com>
2023-11-17 14:17:23 +0000

    Adding script to merge peft adapter

---
+++ src/scripts/slurm/merge_adapter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="merge_adapter"
+#SBATCH --output=/users/lucelo/logs/slurm-%j.out
+#SBATCH --error=/users/lucelo/logs/slurm-%j.err
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+export XDG_CACHE_HOME=/scratch-ssd/oatml/
+
+export TMPDIR=/scratch-ssd/${USER}/tmp
+mkdir -p $TMPDIR
+BUILD_DIR=/scratch-ssd/${USER}/conda_envs/pip-build
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/UQLRM/environment_cloud.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate uqrm
+
+echo $TMPDIR
+
+nvidia-smi
+
+huggingface-cli login --token $HUGGINGFACE_WRITETOKEN
+
+python3 ~/UQLRM/src/scripts/merge_peft_adapter.py \
+--adapter_model_name "luckeciano/llama-sft-reddit" \
+--base_model_name "meta-llama/Llama-2-7b-hf" \
+--output_name "luckeciano/merged-llama-sft-reddit"
